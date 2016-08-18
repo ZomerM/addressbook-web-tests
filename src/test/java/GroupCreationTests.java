1820c429480c539970527aeb4e96c85a24af5d95
ZomerM <mzem@corp.mango.ru>
2016-08-18 19:06:47 +0300

    тест начало

--- src/test/java/GroupCreationTests.java
+++ src/test/java/GroupCreationTests.java
@@ -1,17 +1,27 @@
-import java.util.concurrent.TimeUnit;
-
+import org.junit.After;
+import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.openqa.selenium.OutputType.*;
+
 public class GroupCreationTests {
-    FirefoxDriver wd;
+    ChromeDriver wd;
     
-    @BeforeMethod
+    @Before
     public void setUp() throws Exception {
-        wd = new FirefoxDriver();
+        wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
     
@@ -19,20 +29,15 @@ public class GroupCreationTests {
     public void GroupCreationTests() {
         wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.id("LoginForm")).click();
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        if (!wd.findElement(By.xpath("//form[@id='right']//select[normalize-space(.)='[all] [none] test1']//option[1]")).isSelected()) {
-            wd.findElement(By.xpath("//form[@id='right']//select[normalize-space(.)='[all] [none] test1']//option[1]")).click();
-        }
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("Test");
-        wd.findElement(By.xpath("//div[@id='content']//label[.='Group header (Logo):']")).click();
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
         wd.findElement(By.name("group_name")).sendKeys("Test1");
@@ -41,14 +46,12 @@ public class GroupCreationTests {
         wd.findElement(By.name("group_header")).sendKeys("Test2");
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("Test3");
-        wd.findElement(By.xpath("//div[@id='content']/form")).click();
+        wd.findElement(By.name("group_footer")).sendKeys("test3");
         wd.findElement(By.name("submit")).click();
-        wd.findElement(By.cssSelector("div.msgbox")).click();
         wd.findElement(By.linkText("group page")).click();
     }
     
-    @AfterMethod
+    @After
     public void tearDown() {
         wd.quit();
     }
