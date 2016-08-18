91c7fe0b99b65d66465d5ec4b29877e97e53c9eb
ZomerM <mzem@corp.mango.ru>
2016-08-18 19:41:40 +0300

    Рефакторинг теста

--- src/test/java/GroupCreationTests.java
+++ src/test/java/GroupCreationTests.java
@@ -17,51 +17,76 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTests {
-    ChromeDriver wd;
-    
-    @Before
-    public void setUp() throws Exception {
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTests() {
-        wd.get("http://localhost/addressbook/");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.id("LoginForm")).click();
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.name("new")).click();
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("Test1");
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("Test2");
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("test3");
-        wd.findElement(By.name("submit")).click();
-        wd.findElement(By.linkText("group page")).click();
-    }
-    
-    @After
-    public void tearDown() {
-        wd.quit();
-    }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+	ChromeDriver wd;
+
+	@Before
+	public void setUp() throws Exception {
+		wd = new ChromeDriver();
+		wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		login();
+	}
+
+	public void login() {
+		wd.get("http://localhost/addressbook/");
+		wd.findElement(By.name("pass")).click();
+		wd.findElement(By.name("pass")).clear();
+		wd.findElement(By.name("pass")).sendKeys("secret");
+		wd.findElement(By.name("user")).click();
+		wd.findElement(By.name("user")).clear();
+		wd.findElement(By.name("user")).sendKeys("admin");
+		wd.findElement(By.id("LoginForm")).click();
+		wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+	}
+
+	@Test
+	public void testGroupCreation() {
+
+		gotoGroupPage();
+		initGroupCreation();
+		wd.findElement(By.name("group_name")).click();
+		wd.findElement(By.name("group_name")).clear();
+		fillGroupForm();
+		submitGroupCreation();
+		returnToGroupPage();
+	}
+
+	public void returnToGroupPage() {
+		wd.findElement(By.linkText("group page")).click();
+	}
+
+	public void submitGroupCreation() {
+		wd.findElement(By.name("submit")).click();
+	}
+
+	public void fillGroupForm() {
+		wd.findElement(By.name("group_name")).sendKeys("Test1");
+		wd.findElement(By.name("group_header")).click();
+		wd.findElement(By.name("group_header")).clear();
+		wd.findElement(By.name("group_header")).sendKeys("Test2");
+		wd.findElement(By.name("group_footer")).click();
+		wd.findElement(By.name("group_footer")).clear();
+		wd.findElement(By.name("group_footer")).sendKeys("test3");
+	}
+
+	public void initGroupCreation() {
+		wd.findElement(By.name("new")).click();
+	}
+
+	public void gotoGroupPage() {
+		wd.findElement(By.linkText("groups")).click();
+	}
+
+	@After
+	public void tearDown() {
+		wd.quit();
+	}
+
+	public static boolean isAlertPresent(FirefoxDriver wd) {
+		try {
+			wd.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
 }
