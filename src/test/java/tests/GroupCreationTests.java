981fa7d2d329e92d855b5b2d5b221aa9f6bb1f74
ZomerM <mzem@corp.mango.ru>
2016-08-25 18:44:49 +0300

    Все работает

--- src/test/java/tests/GroupCreationTests.java
+++ src/test/java/tests/GroupCreationTests.java
@@ -1,6 +1,6 @@
 package tests;
+
 import org.junit.Test;
-import org.openqa.selenium.By;
 
 import model.GroupData;
 
@@ -10,8 +10,6 @@ public class GroupCreationTests extends TestBase {
 
 		app.gotoGroupPage();
 		app.initGroupCreation();
-		//wd.findElement(By.name("group_name")).click();
-		//wd.findElement(By.name("group_name")).clear();
 		app.fillGroupForm(new GroupData("Test1", "Test2", "test3"));
 		app.submitGroupCreation();
 		app.returnToGroupPage();
--- src/test/java/tests/GroupDeletionTests.java
+++ src/test/java/tests/GroupDeletionTests.java
@@ -1,19 +1,5 @@
 package tests;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
-
-import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class GroupDeletionTests extends TestBase {
 
